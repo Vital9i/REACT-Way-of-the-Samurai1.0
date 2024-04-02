5b56fd4da9bf2aa08710fe43d27e2cc34482da5a
Vital9i <vitalikdoiniak@gmail.com>
2024-04-02 13:39:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>dbsbdb</li>
-      </ul>
-    </div>
-  );
+    <div>
+      <Header />
+      <Technologies />
+      <Footer />
+      </div>
+      );
 }
 
 export default App;
